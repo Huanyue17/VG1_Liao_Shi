027fe7a88dfab50a5de8ebbc0b2e894cc5913d87
Bohan-Shi <s.bohan@wustl.edu>
2022-10-21 21:23:30 -0500

    Add rating according to complete time, new font, new animals(WIP)

---
+++ Assets/Code/LizardControl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace level1 {
+    public class LizardControl : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
